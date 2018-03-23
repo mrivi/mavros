00a27a86855dcf35efada6680623647508ddb781
Martina <martina@rivizzigno.it>
2018-03-23 09:03:18 +0100

    obstacle_avoidance: use PositionTarget instead of AnchorPoint

--- mavros_extras/src/plugins/obstacle_avoidance.cpp
+++ mavros_extras/src/plugins/obstacle_avoidance.cpp
@@ -16,7 +16,7 @@
 
 #include <mavros/mavros_plugin.h>
 #include <mavros_msgs/ObstacleAvoidance.h>
-#include <mavros_msgs/AnchorPoint.h>
+#include <mavros_msgs/PositionTarget.h>
 
 
 // TODO: create ros message to send obstacle distanc
@@ -74,65 +74,65 @@ private:
                 obstacle_avoidance.type = req->type;    	//!< trajectory type (waypoints, bezier)
 
                 /* conversion between ENU and NED */
-                obstacle_avoidance.point_1[0] = req->point_1.position_setpoint.y;
-                obstacle_avoidance.point_1[1] = req->point_1.position_setpoint.x;
-                obstacle_avoidance.point_1[2] = -req->point_1.position_setpoint.z;
-                obstacle_avoidance.point_1[3] = req->point_1.velocity_setpoint.y;
-                obstacle_avoidance.point_1[4] = req->point_1.velocity_setpoint.x;
-                obstacle_avoidance.point_1[5] = -req->point_1.velocity_setpoint.z;
-                obstacle_avoidance.point_1[6] = req->point_1.acceleration_setpoint.y;
-                obstacle_avoidance.point_1[7] = req->point_1.acceleration_setpoint.x;
-                obstacle_avoidance.point_1[8] = -req->point_1.acceleration_setpoint.z;
+                obstacle_avoidance.point_1[0] = req->point_1.position.y;
+                obstacle_avoidance.point_1[1] = req->point_1.position.x;
+                obstacle_avoidance.point_1[2] = -req->point_1.position.z;
+                obstacle_avoidance.point_1[3] = req->point_1.velocity.y;
+                obstacle_avoidance.point_1[4] = req->point_1.velocity.x;
+                obstacle_avoidance.point_1[5] = -req->point_1.velocity.z;
+                obstacle_avoidance.point_1[6] = req->point_1.acceleration_or_force.y;
+                obstacle_avoidance.point_1[7] = req->point_1.acceleration_or_force.x;
+                obstacle_avoidance.point_1[8] = -req->point_1.acceleration_or_force.z;
                 obstacle_avoidance.point_1[9] = wrap_pi(req->point_1.yaw + (M_PI / 2.0f));
-                obstacle_avoidance.point_1[10] = req->point_1.yaw_speed;
+                obstacle_avoidance.point_1[10] = req->point_1.yaw_rate;
 
-                obstacle_avoidance.point_2[0] = req->point_2.position_setpoint.y;
-                obstacle_avoidance.point_2[1] = req->point_2.position_setpoint.x;
-                obstacle_avoidance.point_2[2] = -req->point_2.position_setpoint.z;
-                obstacle_avoidance.point_2[3] = req->point_2.velocity_setpoint.y;
-                obstacle_avoidance.point_2[4] = req->point_2.velocity_setpoint.x;
-                obstacle_avoidance.point_2[5] = -req->point_2.velocity_setpoint.z;
-                obstacle_avoidance.point_2[6] = req->point_2.acceleration_setpoint.y;
-                obstacle_avoidance.point_2[7] = req->point_2.acceleration_setpoint.x;
-                obstacle_avoidance.point_2[8] = -req->point_2.acceleration_setpoint.z;
+                obstacle_avoidance.point_2[0] = req->point_2.position.y;
+                obstacle_avoidance.point_2[1] = req->point_2.position.x;
+                obstacle_avoidance.point_2[2] = -req->point_2.position.z;
+                obstacle_avoidance.point_2[3] = req->point_2.velocity.y;
+                obstacle_avoidance.point_2[4] = req->point_2.velocity.x;
+                obstacle_avoidance.point_2[5] = -req->point_2.velocity.z;
+                obstacle_avoidance.point_2[6] = req->point_2.acceleration_or_force.y;
+                obstacle_avoidance.point_2[7] = req->point_2.acceleration_or_force.x;
+                obstacle_avoidance.point_2[8] = -req->point_2.acceleration_or_force.z;
                 obstacle_avoidance.point_2[9] = wrap_pi(req->point_2.yaw + (M_PI / 2.0f));
-                obstacle_avoidance.point_2[10] = req->point_2.yaw_speed;
+                obstacle_avoidance.point_2[10] = req->point_2.yaw_rate;
 
-                obstacle_avoidance.point_3[0] = req->point_3.position_setpoint.y;
-                obstacle_avoidance.point_3[1] = req->point_3.position_setpoint.x;
-                obstacle_avoidance.point_3[2] = -req->point_3.position_setpoint.z;
-                obstacle_avoidance.point_3[3] = req->point_3.velocity_setpoint.y;
-                obstacle_avoidance.point_3[4] = req->point_3.velocity_setpoint.x;
-                obstacle_avoidance.point_3[5] = -req->point_3.velocity_setpoint.z;
-                obstacle_avoidance.point_3[6] = req->point_3.acceleration_setpoint.y;
-                obstacle_avoidance.point_3[7] = req->point_3.acceleration_setpoint.x;
-                obstacle_avoidance.point_3[8] = -req->point_3.acceleration_setpoint.z;
+                obstacle_avoidance.point_3[0] = req->point_3.position.y;
+                obstacle_avoidance.point_3[1] = req->point_3.position.x;
+                obstacle_avoidance.point_3[2] = -req->point_3.position.z;
+                obstacle_avoidance.point_3[3] = req->point_3.velocity.y;
+                obstacle_avoidance.point_3[4] = req->point_3.velocity.x;
+                obstacle_avoidance.point_3[5] = -req->point_3.velocity.z;
+                obstacle_avoidance.point_3[6] = req->point_3.acceleration_or_force.y;
+                obstacle_avoidance.point_3[7] = req->point_3.acceleration_or_force.x;
+                obstacle_avoidance.point_3[8] = -req->point_3.acceleration_or_force.z;
                 obstacle_avoidance.point_3[9] = wrap_pi(req->point_3.yaw + (M_PI / 2.0f));
-                obstacle_avoidance.point_3[10] = req->point_3.yaw_speed;
+                obstacle_avoidance.point_3[10] = req->point_3.yaw_rate;
 
-                obstacle_avoidance.point_4[0] = req->point_4.position_setpoint.y;
-                obstacle_avoidance.point_4[1] = req->point_4.position_setpoint.x;
-                obstacle_avoidance.point_4[2] = -req->point_4.position_setpoint.z;
-                obstacle_avoidance.point_4[3] = req->point_4.velocity_setpoint.y;
-                obstacle_avoidance.point_4[4] = req->point_4.velocity_setpoint.x;
-                obstacle_avoidance.point_4[5] = -req->point_4.velocity_setpoint.z;
-                obstacle_avoidance.point_4[6] = req->point_4.acceleration_setpoint.y;
-                obstacle_avoidance.point_4[7] = req->point_4.acceleration_setpoint.x;
-                obstacle_avoidance.point_4[8] = -req->point_4.acceleration_setpoint.z;
+                obstacle_avoidance.point_4[0] = req->point_4.position.y;
+                obstacle_avoidance.point_4[1] = req->point_4.position.x;
+                obstacle_avoidance.point_4[2] = -req->point_4.position.z;
+                obstacle_avoidance.point_4[3] = req->point_4.velocity.y;
+                obstacle_avoidance.point_4[4] = req->point_4.velocity.x;
+                obstacle_avoidance.point_4[5] = -req->point_4.velocity.z;
+                obstacle_avoidance.point_4[6] = req->point_4.acceleration_or_force.y;
+                obstacle_avoidance.point_4[7] = req->point_4.acceleration_or_force.x;
+                obstacle_avoidance.point_4[8] = -req->point_4.acceleration_or_force.z;
                 obstacle_avoidance.point_4[9] = wrap_pi(req->point_4.yaw + (M_PI / 2.0f));
-                obstacle_avoidance.point_4[10] = req->point_4.yaw_speed;
+                obstacle_avoidance.point_4[10] = req->point_4.yaw_rate;
 
-                obstacle_avoidance.point_5[0] = req->point_5.position_setpoint.y;
-                obstacle_avoidance.point_5[1] = req->point_5.position_setpoint.x;
-                obstacle_avoidance.point_5[2] = -req->point_5.position_setpoint.z;
-                obstacle_avoidance.point_5[3] = req->point_5.velocity_setpoint.y;
-                obstacle_avoidance.point_5[4] = req->point_5.velocity_setpoint.x;
-                obstacle_avoidance.point_5[5] = -req->point_5.velocity_setpoint.z;
-                obstacle_avoidance.point_5[6] = req->point_5.acceleration_setpoint.y;
-                obstacle_avoidance.point_5[7] = req->point_5.acceleration_setpoint.x;
-                obstacle_avoidance.point_5[8] = -req->point_5.acceleration_setpoint.z;
+                obstacle_avoidance.point_5[0] = req->point_5.position.y;
+                obstacle_avoidance.point_5[1] = req->point_5.position.x;
+                obstacle_avoidance.point_5[2] = -req->point_5.position.z;
+                obstacle_avoidance.point_5[3] = req->point_5.velocity.y;
+                obstacle_avoidance.point_5[4] = req->point_5.velocity.x;
+                obstacle_avoidance.point_5[5] = -req->point_5.velocity.z;
+                obstacle_avoidance.point_5[6] = req->point_5.acceleration_or_force.y;
+                obstacle_avoidance.point_5[7] = req->point_5.acceleration_or_force.x;
+                obstacle_avoidance.point_5[8] = -req->point_5.acceleration_or_force.z;
                 obstacle_avoidance.point_5[9] = wrap_pi(req->point_5.yaw + (M_PI / 2.0f));
-                obstacle_avoidance.point_5[10] = req->point_5.yaw_speed;
+                obstacle_avoidance.point_5[10] = req->point_5.yaw_rate;
 
         	std::copy(req->point_valid.begin(), req->point_valid.end(), obstacle_avoidance.point_valid.begin());
         	std::copy(req->field_of_view.begin(), req->field_of_view.end(), obstacle_avoidance.field_of_view.begin());
@@ -163,65 +163,65 @@ private:
                 auto obstacle_avoidance_input = boost::make_shared<mavros_msgs::ObstacleAvoidance>();
                 obstacle_avoidance_input->header = m_uas->synchronized_header("local_origin", avoid_input.time_usec);
 
-                obstacle_avoidance_input->point_1.position_setpoint.x = avoid_input.point_1[1];
-                obstacle_avoidance_input->point_1.position_setpoint.y = avoid_input.point_1[0];
-                obstacle_avoidance_input->point_1.position_setpoint.z = -avoid_input.point_1[2];
-                obstacle_avoidance_input->point_1.velocity_setpoint.x = avoid_input.point_1[4];
-                obstacle_avoidance_input->point_1.velocity_setpoint.y = avoid_input.point_1[3];
-                obstacle_avoidance_input->point_1.velocity_setpoint.z = -avoid_input.point_1[5];
-                obstacle_avoidance_input->point_1.acceleration_setpoint.x = avoid_input.point_1[7];
-                obstacle_avoidance_input->point_1.acceleration_setpoint.y = avoid_input.point_1[6];
-                obstacle_avoidance_input->point_1.acceleration_setpoint.z = -avoid_input.point_1[8];
+                obstacle_avoidance_input->point_1.position.x = avoid_input.point_1[1];
+                obstacle_avoidance_input->point_1.position.y = avoid_input.point_1[0];
+                obstacle_avoidance_input->point_1.position.z = -avoid_input.point_1[2];
+                obstacle_avoidance_input->point_1.velocity.x = avoid_input.point_1[4];
+                obstacle_avoidance_input->point_1.velocity.y = avoid_input.point_1[3];
+                obstacle_avoidance_input->point_1.velocity.z = -avoid_input.point_1[5];
+                obstacle_avoidance_input->point_1.acceleration_or_force.x = avoid_input.point_1[7];
+                obstacle_avoidance_input->point_1.acceleration_or_force.y = avoid_input.point_1[6];
+                obstacle_avoidance_input->point_1.acceleration_or_force.z = -avoid_input.point_1[8];
                 obstacle_avoidance_input->point_1.yaw = wrap_pi(avoid_input.point_1[9] - (M_PI / 2.0f));
-                obstacle_avoidance_input->point_1.yaw_speed = avoid_input.point_1[10];
+                obstacle_avoidance_input->point_1.yaw_rate = avoid_input.point_1[10];
 
-                obstacle_avoidance_input->point_2.position_setpoint.x = avoid_input.point_2[1];
-                obstacle_avoidance_input->point_2.position_setpoint.y = avoid_input.point_2[0];
-                obstacle_avoidance_input->point_2.position_setpoint.z = -avoid_input.point_2[2];
-                obstacle_avoidance_input->point_2.velocity_setpoint.x = avoid_input.point_2[4];
-                obstacle_avoidance_input->point_2.velocity_setpoint.y = avoid_input.point_2[3];
-                obstacle_avoidance_input->point_2.velocity_setpoint.z = -avoid_input.point_2[5];
-                obstacle_avoidance_input->point_2.acceleration_setpoint.x = avoid_input.point_2[7];
-                obstacle_avoidance_input->point_2.acceleration_setpoint.y = avoid_input.point_2[6];
-                obstacle_avoidance_input->point_2.acceleration_setpoint.z = -avoid_input.point_2[8];
+                obstacle_avoidance_input->point_2.position.x = avoid_input.point_2[1];
+                obstacle_avoidance_input->point_2.position.y = avoid_input.point_2[0];
+                obstacle_avoidance_input->point_2.position.z = -avoid_input.point_2[2];
+                obstacle_avoidance_input->point_2.velocity.x = avoid_input.point_2[4];
+                obstacle_avoidance_input->point_2.velocity.y = avoid_input.point_2[3];
+                obstacle_avoidance_input->point_2.velocity.z = -avoid_input.point_2[5];
+                obstacle_avoidance_input->point_2.acceleration_or_force.x = avoid_input.point_2[7];
+                obstacle_avoidance_input->point_2.acceleration_or_force.y = avoid_input.point_2[6];
+                obstacle_avoidance_input->point_2.acceleration_or_force.z = -avoid_input.point_2[8];
                 obstacle_avoidance_input->point_2.yaw = wrap_pi(avoid_input.point_2[9] - (M_PI / 2.0f));
-                obstacle_avoidance_input->point_2.yaw_speed = avoid_input.point_2[10];
+                obstacle_avoidance_input->point_2.yaw_rate = avoid_input.point_2[10];
 
-                obstacle_avoidance_input->point_3.position_setpoint.x = avoid_input.point_3[1];
-                obstacle_avoidance_input->point_3.position_setpoint.y = avoid_input.point_3[0];
-                obstacle_avoidance_input->point_3.position_setpoint.z = -avoid_input.point_3[2];
-                obstacle_avoidance_input->point_3.velocity_setpoint.x = avoid_input.point_3[4];
-                obstacle_avoidance_input->point_3.velocity_setpoint.y = avoid_input.point_3[3];
-                obstacle_avoidance_input->point_3.velocity_setpoint.z = -avoid_input.point_3[5];
-                obstacle_avoidance_input->point_3.acceleration_setpoint.x = avoid_input.point_3[7];
-                obstacle_avoidance_input->point_3.acceleration_setpoint.y = avoid_input.point_3[6];
-                obstacle_avoidance_input->point_3.acceleration_setpoint.z = -avoid_input.point_3[8];
+                obstacle_avoidance_input->point_3.position.x = avoid_input.point_3[1];
+                obstacle_avoidance_input->point_3.position.y = avoid_input.point_3[0];
+                obstacle_avoidance_input->point_3.position.z = -avoid_input.point_3[2];
+                obstacle_avoidance_input->point_3.velocity.x = avoid_input.point_3[4];
+                obstacle_avoidance_input->point_3.velocity.y = avoid_input.point_3[3];
+                obstacle_avoidance_input->point_3.velocity.z = -avoid_input.point_3[5];
+                obstacle_avoidance_input->point_3.acceleration_or_force.x = avoid_input.point_3[7];
+                obstacle_avoidance_input->point_3.acceleration_or_force.y = avoid_input.point_3[6];
+                obstacle_avoidance_input->point_3.acceleration_or_force.z = -avoid_input.point_3[8];
                 obstacle_avoidance_input->point_3.yaw = wrap_pi(avoid_input.point_3[9] - (M_PI / 2.0f));
-                obstacle_avoidance_input->point_3.yaw_speed = avoid_input.point_3[10];
+                obstacle_avoidance_input->point_3.yaw_rate = avoid_input.point_3[10];
 
-                obstacle_avoidance_input->point_4.position_setpoint.x = avoid_input.point_4[1];
-                obstacle_avoidance_input->point_4.position_setpoint.y = avoid_input.point_4[0];
-                obstacle_avoidance_input->point_4.position_setpoint.z = -avoid_input.point_4[2];
-                obstacle_avoidance_input->point_4.velocity_setpoint.x = avoid_input.point_4[4];
-                obstacle_avoidance_input->point_4.velocity_setpoint.y = avoid_input.point_4[3];
-                obstacle_avoidance_input->point_4.velocity_setpoint.z = -avoid_input.point_4[5];
-                obstacle_avoidance_input->point_4.acceleration_setpoint.x = avoid_input.point_4[7];
-                obstacle_avoidance_input->point_4.acceleration_setpoint.y = avoid_input.point_4[6];
-                obstacle_avoidance_input->point_4.acceleration_setpoint.z = -avoid_input.point_4[8];
+                obstacle_avoidance_input->point_4.position.x = avoid_input.point_4[1];
+                obstacle_avoidance_input->point_4.position.y = avoid_input.point_4[0];
+                obstacle_avoidance_input->point_4.position.z = -avoid_input.point_4[2];
+                obstacle_avoidance_input->point_4.velocity.x = avoid_input.point_4[4];
+                obstacle_avoidance_input->point_4.velocity.y = avoid_input.point_4[3];
+                obstacle_avoidance_input->point_4.velocity.z = -avoid_input.point_4[5];
+                obstacle_avoidance_input->point_4.acceleration_or_force.x = avoid_input.point_4[7];
+                obstacle_avoidance_input->point_4.acceleration_or_force.y = avoid_input.point_4[6];
+                obstacle_avoidance_input->point_4.acceleration_or_force.z = -avoid_input.point_4[8];
                 obstacle_avoidance_input->point_4.yaw = wrap_pi(avoid_input.point_4[9] - (M_PI / 2.0f));
-                obstacle_avoidance_input->point_4.yaw_speed = avoid_input.point_4[10];
+                obstacle_avoidance_input->point_4.yaw_rate = avoid_input.point_4[10];
 
-                obstacle_avoidance_input->point_5.position_setpoint.x = avoid_input.point_5[1];
-                obstacle_avoidance_input->point_5.position_setpoint.y = avoid_input.point_5[0];
-                obstacle_avoidance_input->point_5.position_setpoint.z = -avoid_input.point_5[2];
-                obstacle_avoidance_input->point_5.velocity_setpoint.x = avoid_input.point_5[4];
-                obstacle_avoidance_input->point_5.velocity_setpoint.y = avoid_input.point_5[3];
-                obstacle_avoidance_input->point_5.velocity_setpoint.z = -avoid_input.point_5[5];
-                obstacle_avoidance_input->point_5.acceleration_setpoint.x = avoid_input.point_5[7];
-                obstacle_avoidance_input->point_5.acceleration_setpoint.y = avoid_input.point_5[6];
-                obstacle_avoidance_input->point_5.acceleration_setpoint.z = -avoid_input.point_5[8];
+                obstacle_avoidance_input->point_5.position.x = avoid_input.point_5[1];
+                obstacle_avoidance_input->point_5.position.y = avoid_input.point_5[0];
+                obstacle_avoidance_input->point_5.position.z = -avoid_input.point_5[2];
+                obstacle_avoidance_input->point_5.velocity.x = avoid_input.point_5[4];
+                obstacle_avoidance_input->point_5.velocity.y = avoid_input.point_5[3];
+                obstacle_avoidance_input->point_5.velocity.z = -avoid_input.point_5[5];
+                obstacle_avoidance_input->point_5.acceleration_or_force.x = avoid_input.point_5[7];
+                obstacle_avoidance_input->point_5.acceleration_or_force.y = avoid_input.point_5[6];
+                obstacle_avoidance_input->point_5.acceleration_or_force.z = -avoid_input.point_5[8];
                 obstacle_avoidance_input->point_5.yaw = wrap_pi(avoid_input.point_5[9] - (M_PI / 2.0f));
-                obstacle_avoidance_input->point_5.yaw_speed = avoid_input.point_5[10];
+                obstacle_avoidance_input->point_5.yaw_rate = avoid_input.point_5[10];
 
                 std::copy(avoid_input.point_valid.begin(), avoid_input.point_valid.end(), obstacle_avoidance_input->point_valid.begin());
                 std::copy(avoid_input.field_of_view.begin(), avoid_input.field_of_view.end(), obstacle_avoidance_input->field_of_view.begin());
